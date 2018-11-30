06cb8f319edb16b4b2f083d1f009bedbfc849422
Unknown <beckettmorsch@gmail.com>
2018-11-30 15:47:58 -0500

    Tweaked colors

--- QuizzicalFBLA/QuizzicalFBLA.Android/MainActivity.cs
+++ QuizzicalFBLA/QuizzicalFBLA.Android/MainActivity.cs
@@ -12,7 +12,7 @@ using LabelHtml.Forms.Plugin.Droid;
 
 namespace QuizzicalFBLA.Droid
 {
-    [Activity(Label = "QuizzicalFBLA", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "QuizzicalFBLA", Icon = "@mipmap/ic_launcher", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
