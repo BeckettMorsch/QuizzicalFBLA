17c95700d6de11d87f41bc678e9f7c47017f8663
Unknown <beckettmorsch@gmail.com>
2018-12-05 11:50:56 -0500

    Small fixes

--- QuizzicalFBLA/QuizzicalFBLA/Views/MenuPage.xaml.cs
+++ QuizzicalFBLA/QuizzicalFBLA/Views/MenuPage.xaml.cs
@@ -38,6 +38,8 @@ namespace QuizzicalFBLA.Views
 
                 ListViewMenu.SelectedItem = null;
 
+                RootPage.IsPresented = false;
+
                 await RootPage.NavigateFromMenu(id);
             };
         }
